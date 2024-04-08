608180a287d8f21a5202769ce08f13690677764a
giosciafra <francescogioscia8@gmail.com>
2024-04-08 18:00:00 +0200

    aggiunte immagini e iniziato header

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -10,8 +10,7 @@
 </head>
 <body>
 
-        @yield('content')
-
+    @include('partials/header')
 
   
 </body>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,17 @@
+<nav>
+    <div class="container">
+        <img class="logo" src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+        <ul class="links">
+            <li>characters</li>
+            <li class="active">comics</li>
+            <li>movies</li>
+            <li>tv</li>
+            <li>games</li>
+            <li>collectibles</li>
+            <li>videos</li>
+            <li>fans</li>
+            <li>news</li>
+            <li>shop</li>
+        </ul>
+    </div>
+</nav>
